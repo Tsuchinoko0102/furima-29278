d1f50649e6ede67d29439c7713dbf327abfaafb5
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-09-10 14:33:48 +0900

    バリデーションセット / new_item_pathへのルーティング

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,25 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :duration
+  
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+    validates :user
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :duration_id
+  end
+
+  with_options numericality: {other_than: 1} do
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :duration_id
+  end
+
 end
