e1a9801ccfe79eddfe202b752404da4938319b26
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-09-10 12:36:42 +0900

    User-Items-AH Association定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensitons
+  belongs_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :duration
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
   READING_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   PASS_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
