9155a56c942c7f95a888094cdd7198899b520cd9
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-09-10 18:21:18 +0900

    parameters受取OK→DB保存確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create 
+    @item = Item.create(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  def item_params
+    params.require(:item)
+    .permit(:name, :text, :price, :user, :category_id, :status_id, :fee_id, :prefecture_id, :duration_id, :image)
+    .merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensitons
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :duration
+  has_one_attached :image
   
   with_options presence: true do
     validates :name
