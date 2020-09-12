3e15f4dc4d3546b8f2a69e7b305fd85848f9d82a
Tsuchinoko0102 <tsuchinoko.com@gmail.com>
2020-09-12 16:04:07 +0900

    一覧表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
-  resources :items, only:[:index, :new, :create]
+  resources :items, only:[:index, :new, :create, :show, :edit]
   devise_for :users
 end
